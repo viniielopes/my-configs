97f39e3d3db067fd986ebfd2ceae10b2af1e8db6
Vinicius Lopes <viniielopes@live.com>
2023-11-05 19:40:50 -0300

    add xrandr to change monitor side

--- dwm/scripts/run.sh
+++ dwm/scripts/run.sh
@@ -4,6 +4,7 @@ xrdb merge ~/.Xresources
 xbacklight -set 10 &
 feh --bg-max -z ~/Downloads/wallpapers/ &
 xset r rate 200 50 &
+xrandr --output HDMI-A-0 --left-of DisplayPort-2 &
 picom &
 
 ~/.config/chadwm/scripts/bar.sh &
